9b133cf7cf11d41da14605ab7f6eca705f1a7547
Shayan <sh0davoodi@gmail.com>
2023-11-14 11:02:41 +0330

    [make.jl] `checkdocs=:exports` added (desc 📄) + This modification involves notifying Documenter.jl not to enforce strict rules on unused docstrings.

--- docs/make.jl
+++ docs/make.jl
@@ -2,16 +2,20 @@ using Documenter
 push!(LOAD_PATH,"../src/")
 using EmpiricalModeDecomposition
 
-makedocs(sitename="EmpiricalModeDecomposition.jl Documentation",
-        pages = [
-                 "Library" => [
-                    "Reference" => "reference.md",
-                    "Extras" => "extras.md",
-                    "Internals" => "internals.md"
-                    ],
-                "Methods" => "methods.md"
-              ]
-       )
+makedocs(;
+  sitename="EmpiricalModeDecomposition.jl",
+  checkdocs=:exports,
+  pages = Any[
+    "Home" => "index.md",
+    "Library" => [
+      "Reference" => "reference.md",
+      "Extras" => "extras.md",
+      "Internals" => "internals.md"
+    ],
+    "Methods" => "methods.md"
+  ]
+)
+
 deploydocs(
-           repo = "github.com/atmnpatel/EmpiricalModeDecomposition.jl.git"
+  repo = "github.com/atmnpatel/EmpiricalModeDecomposition.jl.git"
 )
